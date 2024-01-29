277a48c81a8c3c9edbeec691b002b5a042b63f94
Vojtěch Coufal <vojtech.cou@gmail.com>
2024-01-29 19:17:02 +0100

    First layout

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,35 @@ fn handle_events() -> io::Result<bool> {
 }
 
 fn ui(frame: &mut Frame) {
+    let main_layout = Layout::new(
+        Direction::Vertical,
+        [
+            Constraint::Length(1),
+            Constraint::Min(0),
+            Constraint::Length(1),
+        ],
+    )
+    .split(frame.size());
     frame.render_widget(
-        Paragraph::new("Lets go!!!").block(Block::default().title("Titulek").borders(Borders::ALL)),
-        frame.size(),
+        Block::new().borders(Borders::TOP).title("Vocofo"),
+        main_layout[0],
+    );
+    frame.render_widget(
+        Block::new().borders(Borders::TOP).title("Status Bar"),
+        main_layout[2],
+    );
+
+    let inner_layout = Layout::new(
+        Direction::Horizontal,
+        [Constraint::Percentage(50), Constraint::Percentage(50)],
+    )
+    .split(main_layout[1]);
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("Left"),
+        inner_layout[0],
+    );
+    frame.render_widget(
+        Block::default().borders(Borders::ALL).title("Right"),
+        inner_layout[1],
     );
 }
